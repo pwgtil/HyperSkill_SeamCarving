49aa7ec7b927851d766f7b1207c070125325b298
pwgtiluser.email <87475830+pwgtil@users.noreply.github.com>
2023-02-21 14:12:43 +0100

    Base preparation for stage 5

--- Seam Carving/task/src/seamcarving/ImageOps.kt
+++ Seam Carving/task/src/seamcarving/ImageOps.kt
@@ -6,14 +6,13 @@ import java.io.File
 import javax.imageio.ImageIO
 import kotlin.math.sqrt
 
-
 class Model {
-    companion object {
-
-    }
-
+    // objects properties
     private val msgList = mutableListOf<Messages>()
 
+    //------------------------------------------------------------------------------------------------------------------
+    // File operations: File -> Image and Image -> File
+    //------------------------------------------------------------------------------------------------------------------
     fun getOriginalImage(path: String): BufferedImage? {
         val fileExists = File(path).run { exists() && isFile }
         val fileIsImage = path.substring(path.length - 4, path.length) in listOf(".jpg", ".png")
@@ -27,132 +26,70 @@ class Model {
         return null
     }
 
-    fun createImage(width: Int, height: Int): BufferedImage {
-        return BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
-    }
-
     fun saveImageFile(outputImage: BufferedImage, path: String) {
         ImageIO.write(outputImage, "png", File(path))
     }
 
-    fun getIntensityMatrix(image: BufferedImage): MutableList<MutableList<Int>> {
-        return this.fromEnergyToIntensity(this.buildEnergyMatrix(image))
+    fun createImage(width: Int, height: Int): BufferedImage {
+        return BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
     }
+    //------------------------------------------------------------------------------------------------------------------
 
-    fun calculateShortestPath(
+    //------------------------------------------------------------------------------------------------------------------
+    // Core public methods. Get path, get energy matrix, get adjacency matrix
+    //------------------------------------------------------------------------------------------------------------------
+    fun getPath(
         energyMatrix: MutableList<MutableList<Double>>,
-        adjacencyMatrix: List<MutableList<MutableList<Position>>>
-    ): Unit {
-
-    }
-
-    fun generateAdjacencyMatrix(intensityMatrix: MutableList<MutableList<Int>>): List<MutableList<MutableList<Position>>> {
-        val matrix = mutableListOf<MutableList<MutableList<Position>>>()
-        val baseNode = mutableListOf<MutableList<Position>>(mutableListOf())
-        for (y in 0 until intensityMatrix.size) {
-            matrix.add(mutableListOf())
-            // Base node adjacent to all nodes in the first vertical line
-            baseNode[0].add(Position(0, y))
-            for (x in 0 until intensityMatrix[0].size - 1) {
-                val edges = mutableListOf<Position>()
-                val minimumHeight = (y - 1).coerceAtLeast(0)
-                val maximumHeight = (y + 1).coerceAtMost(intensityMatrix.size - 1)
-                for (e in minimumHeight..maximumHeight) {
-                    edges.add(Position(x + 1, e))
-                }
-                matrix[y].add(edges)
-            }
-            // All nodes in the last vertical line should point on the final node (marked with position Int.Max)
-            matrix[y].add(mutableListOf(Position(Int.MAX_VALUE, Int.MAX_VALUE)))
-        }
-        matrix.add(baseNode)
-        return matrix.toList()
-    }
-
-    fun testDijkstra(path: String): MutableList<Node> {
-        val image = this.getOriginalImage(path)!!
-        val intensityMatrix = this.getIntensityMatrix(image)
-        val adjacencyMatrix = this.generateAdjacencyMatrix(intensityMatrix)
-        return dijkstraAlgorithm(intensityMatrix, adjacencyMatrix)
-    }
-
-    fun dijkstraAlgorithm(
-        intensityMatrix: List<MutableList<Int>>,
-        adjacencyMatrix: List<MutableList<MutableList<Position>>>
-    ): MutableList<Node> {
-//    ): List<Position> {
-        var baseNode = Node(Position(0, adjacencyMatrix.lastIndex), 0)
-        val nodes = mutableListOf<Node>(baseNode)
-
-        while (true) {
-            // 1. Find first unprocessed node
-            val currentNode =
-                nodes.filter { !it.processed && it.nodeLocation.x != Int.MAX_VALUE }.minByOrNull { it.minDistance }
-
-            if (currentNode != null) {
-
-                // 2. Find all unprocessed neighbors
-                val neighbors = adjacencyMatrix[currentNode.nodeLocation.y][currentNode.nodeLocation.x]
-
-                // 3. Process neighbors
-                neighbors.forEach {
-
-                    // 3a. Get distance from current node to neighbor
-                    val distanceToNeighbor =
-                        if (it.y in intensityMatrix.indices && it.x in intensityMatrix[0].indices) {
-                            intensityMatrix[it.y][it.x]
-                        } else {
-                            0
+        workMatrix: MutableList<MutableList<Node>>
+    ): MutableList<Position>? {
+        // 1. Loop at every row of the image and calculate the lowest energy neighbors (top to bottom)
+        for (y in 0 until energyMatrix.size) {
+            val yPrim = y - 1
+            for (x in 0 until energyMatrix[y].size) {
+                val currentNodeEnergy = energyMatrix[y][x]
+                if (y == 0) {
+                    workMatrix[y][x].minDistance = currentNodeEnergy
+                } else {
+                    for (xPrim in (x - 1).coerceAtLeast(0)..
+                            (x + 1).coerceAtMost(energyMatrix[y].lastIndex)) {
+                        val pretendingDistance = workMatrix[yPrim][xPrim].minDistance + currentNodeEnergy
+                        if (pretendingDistance < workMatrix[y][x].minDistance) {
+                            workMatrix[y][x].minDistance = pretendingDistance
+                            workMatrix[y][x].nodePredecessor = workMatrix[yPrim][xPrim]
                         }
-
-                    // 3b. Check if neighbor is already on the list of nodes to process
-                    var neighbor = nodes.find { node -> node.nodeLocation.x == it.x && node.nodeLocation.y == it.y }
-                    if (neighbor != null) {
-
-                        // 3b'. Neighbor already on the list, let's check if we have better distance to it
-                        if (!neighbor.processed && neighbor.minDistance > currentNode.minDistance + distanceToNeighbor) {
-                            neighbor.minDistance = currentNode.minDistance + distanceToNeighbor
-                        }
-                    } else {
-
-                        // 3b''. Neighbor not on the list yet, let's create new entry and add it to the list
-                        neighbor =
-                            Node(Position(it.x, it.y), currentNode.minDistance + distanceToNeighbor, false, currentNode)
-                        nodes.add(neighbor)
                     }
                 }
-
-                // 4. All neighbors are considered -> this node is processed
-                currentNode.processed = true
-
-            } else {
-                // 5. There is no more unprocessed nodes end of the story
-                break
             }
         }
-        return nodes
 
-    }
-
-    private fun fromEnergyToIntensity(energyMatrix: MutableList<MutableList<Double>>): MutableList<MutableList<Int>> {
-        val maxEnergyValue = getMaxEnergyValue(energyMatrix)
-        val intensityMatrix = mutableListOf<MutableList<Int>>()
-
-        for (h in 0 until energyMatrix.size) {
-            intensityMatrix.add(mutableListOf())
-            for (w in 0 until energyMatrix[h].size) {
-                intensityMatrix[h].add((255.0 * energyMatrix[h][w] / maxEnergyValue).toInt())
+        // 2. Traverse back from the lowest energy pixel from the bottom side to the top - get path
+        val lastBestNode = workMatrix[workMatrix.lastIndex].minByOrNull { it.minDistance }
+        return if (lastBestNode != null) {
+            val seamPath = mutableListOf<Position>()
+            var currentNode = lastBestNode!!
+            while (true) {
+                seamPath.add(currentNode.position)
+                if (currentNode.nodePredecessor != null) {
+                    currentNode = currentNode.nodePredecessor!!
+                } else {
+                    break
+                }
             }
+            seamPath
+        } else {
+            null
         }
-        return intensityMatrix
     }
 
-    private fun getMaxEnergyValue(energyMatrix: MutableList<MutableList<Double>>): Double {
-        return energyMatrix.maxOf { it.maxOrNull() ?: Double.MAX_VALUE }
+    fun drawSeam(image: BufferedImage, seamPath: MutableList<Position>): BufferedImage {
+        val tempImage = imageDeepCopy(image)
+        seamPath.forEach {
+            tempImage.setRGB(it.x, it.y, Color.RED.rgb)
+        }
+        return tempImage
     }
 
-
-    private fun buildEnergyMatrix(image: BufferedImage): MutableList<MutableList<Double>> {
+    fun buildEnergyMatrix(image: BufferedImage): MutableList<MutableList<Double>> {
         val energyMatrix = mutableListOf<MutableList<Double>>()
         for (h in 0 until image.height) {
             energyMatrix.add(mutableListOf())
@@ -163,6 +100,20 @@ class Model {
         return energyMatrix
     }
 
+    fun buildWorkMatrix(image: BufferedImage): MutableList<MutableList<Node>> {
+        val workMatrix = mutableListOf<MutableList<Node>>()
+        for (h in 0 until image.height) {
+            workMatrix.add(mutableListOf())
+            for (w in 0 until image.width) {
+                workMatrix[h].add(Node(Position(w, h)))
+            }
+        }
+        return workMatrix
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+    // Internal private methods
+    //------------------------------------------------------------------------------------------------------------------
     private fun calculateEnergy(height: Int, width: Int, image: BufferedImage): Double {
         return sqrt(
             (calculateWidthComponent(height, width, image) + calculateHeightComponent(height, width, image)).toDouble()
@@ -199,6 +150,7 @@ class Model {
         val raster = bi.copyData(null)
         return BufferedImage(cm, raster, isAlpha, null)
     }
+    //------------------------------------------------------------------------------------------------------------------
 
     inline fun <reified T> transpose(xs: Array<Array<T>>): Array<Array<T>> {
         val cols = xs[0].size
@@ -210,48 +162,12 @@ class Model {
         }
     }
 
-    //    fun drawCross(image: BufferedImage): BufferedImage {
-//
-//        val processedImage = imageDeepCopy(image)
-//        val graphics = processedImage.graphics
-//        graphics.color = Color.red
-//        graphics.drawLine(0, 0, processedImage.width - 1, processedImage.height - 1)
-//        graphics.drawLine(processedImage.width - 1, 0, 0, processedImage.height - 1)
-//        graphics.dispose()
-//        return processedImage
-//    }
-//
-//    fun drawNegative(image: BufferedImage): BufferedImage {
-//        val processedImage = imageDeepCopy(image)
-//        for (h in 0 until processedImage.height) {
-//            for (w in 0 until processedImage.width) {
-//                val color = Color(image.getRGB(w, h)).run { Color(255 - red, 255 - green, 255 - blue) }
-//                processedImage.setRGB(w, h, color.rgb)
-//            }
-//        }
-//        return processedImage
-//    }
 //    fun addMessage(message: String, msgCode: Int) {
 //        // msgCode description
 //        // 100 - 199 -> Info
 //        // 500 - 599 -> Warning
 //        // 600 - 699 -> Error
 //        msgList.add(Messages(message, msgCode))
-//    }
-//
-//    fun drawIntensity(image: BufferedImage): BufferedImage {
-//        val processedImage = imageDeepCopy(image)
-//        val energyMatrix = buildEnergyMatrix(processedImage)
-//        val intensityMatrix = fromEnergyToIntensity(energyMatrix)
-//
-//        for (h in 0 until processedImage.height) {
-//            for (w in 0 until processedImage.width) {
-//                val intensity = intensityMatrix[h][w]
-//                val color = Color(intensity, intensity, intensity)
-//                processedImage.setRGB(w, h, color.rgb)
-//            }
-//        }
-//        return processedImage
 //    }
 
 }
@@ -261,8 +177,7 @@ data class Messages(val message: String, val msgCode: Int)
 data class Position(val x: Int, val y: Int)
 
 data class Node(
-    val nodeLocation: Position,
-    var minDistance: Int = Int.MAX_VALUE,
-    var processed: Boolean = false,
+    val position: Position,
+    var minDistance: Double = Double.MAX_VALUE,
     var nodePredecessor: Node? = null
 )
--- Seam Carving/task/src/seamcarving/Main.kt
+++ Seam Carving/task/src/seamcarving/Main.kt
@@ -6,31 +6,26 @@ fun main(args : Array<String>) {
 
 class View {
     companion object {
-
-
 //        const val MSG_ENTER_RECT_WIDTH = "Enter rectangle width:"
 //        const val MSG_ENTER_RECT_HEIGHT = "Enter rectangle height:"
 //        const val MSG_ENTER_OUTPUT_IMAGE_NAME = "Enter output image name:"
 
         fun start(args : Array<String>) {
 
-            val width: Int
-            val height: Int
             val inputPath: String = args[1]
             val outputPath: String = args[3]
 
-
-
             val model = Model()
-
-            val test = model.testDijkstra(inputPath)
-            println()
-
-//            model.saveImageFile(model.drawIntensity(model.getOriginalImage(inputPath)!!), outputPath)
-
-//            model.saveImageFile(model.drawCross(model.createImage(width, height)), path)
-
+            val image = model.getOriginalImage(inputPath)
+            if (image != null) {
+                val energyMatrix = image.let { model.buildEnergyMatrix(it) }
+                val workMatrix = image.let { model.buildWorkMatrix(it) }
+                val path = model.getPath(energyMatrix, workMatrix)
+                if (path != null) {
+                    val imageWithSeam = model.drawSeam(image, path)
+                    model.saveImageFile(imageWithSeam, outputPath)
+                }
+            }
         }
-
     }
 }
